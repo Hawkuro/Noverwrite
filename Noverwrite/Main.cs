9ee273a5a415c57ee93dbf8d9f8dfd912ac6397f
Hawkuro <hawkuro@gmail.com>
2016-04-20 18:31:23 +0000

    Config reading works

--- Noverwrite/Main.cs
+++ Noverwrite/Main.cs
@@ -1,21 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Script.Serialization;
 using StardewModdingAPI;
 
 namespace Noverwrite
 {
-    public static class StaticInfo
+    public class ConfigObject
+    {
+        public string SaveFolder { get; set; }
+    }
+
+    public static class Constants
     {
         public static readonly string MOD_NAME = "Noverwrite by Hawkuro";
+        public static readonly string MOD_FOLDER = "Mods\\Noverwrite";
     }
     public class Main : Mod
     {
+        private ConfigObject config;
         public override void Entry(params object[] objects)
         {
-            Log.Info(StaticInfo.MOD_NAME + " => Initialized");
+            Log.Info(Constants.MOD_NAME + " => Initializing");
+
+            config = new JavaScriptSerializer().Deserialize<ConfigObject>(new StreamReader(Constants.MOD_FOLDER+"\\config.json").ReadToEnd());
+            //Log.Info(config.SaveFolder);
         }
     }
 }
