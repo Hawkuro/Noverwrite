9bac5ee13aa39a93915be8867b9ded70df24d59a
Hawkuro <hawkuro@gmail.com>
2016-04-20 17:59:45 +0000

    Init, basic mod doing nothing

---
+++ Noverwrite/Main.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StardewModdingAPI;
+
+namespace Noverwrite
+{
+    public static class StaticInfo
+    {
+        public static readonly string MOD_NAME = "Noverwrite by Hawkuro";
+    }
+    public class Main : Mod
+    {
+        public override void Entry(params object[] objects)
+        {
+            Log.Info(StaticInfo.MOD_NAME + " => Initialized");
+        }
+    }
+}
